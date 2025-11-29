4e49da0652dd4692235f07d31fd0e8b7237f9f07
davidhrabcak <davidko.hrabcak@gmail.com>
2025-11-29 20:08:51 +0100

    add unit tests for effects, hotfix some errors that prevent you from running tests, fix effects, add new constructors

--- src/main/java/com/terrafutura/board/Grid.java
+++ src/main/java/com/terrafutura/board/Grid.java
@@ -2,9 +2,14 @@ package main.java.com.terrafutura.board;
 
 import main.java.com.terrafutura.cards.Card;
 
+import java.util.List;
 import java.util.Optional;
 
 public class Grid {
     public Optional<Card> putCard(GridPosition coordinate, Card card) { return Optional.empty(); }
     public boolean canPutCard(GridPosition coordinate) { return false; }
+
+    public Optional<Card> getCard(GridPosition position) {
+        return Optional.empty();
+    }
 }
--- src/test/java/effect/ArbitraryBasicTest.java
+++ src/test/java/effect/ArbitraryBasicTest.java
@@ -19,28 +19,64 @@ public class ArbitraryBasicTest {
 
     @Test
     public void testValidTransaction() {
-        ArbitraryBasic effect = new ArbitraryBasic(Arrays.asList(car));
+        ArbitraryBasic effect = new ArbitraryBasic(List.of(car));
 
-        List<Resource> input = Arrays.asList(bulb, bulb); // arbitrary
+        List<Resource> input = List.of(bulb, bulb);
         List<Resource> output = new ArrayList<>();
 
         boolean ok = effect.check(input, output, 0);
 
         assertTrue(ok);
-        assertEquals(1, output.size());
-        assertEquals(car, output.getFirst());
+    }
+
+    @Test
+    public void testWrongInput() {
+        ArbitraryBasic effect = new ArbitraryBasic(List.of(car));
+
+        List<Resource> output = new ArrayList<>();
+
+        boolean wrongIn = effect.check(List.of(), output, 0);
+
+        assertFalse(wrongIn);
+    }
+
+    @Test
+    public void testWrongOutput() {
+        ArbitraryBasic effect = new ArbitraryBasic(List.of(car));
+
+        List<Resource> input = List.of(bulb, bulb);
+
+        boolean wrongOut = effect.check(input, List.of(bulb, bulb, bulb), 0);
+
+        assertFalse(wrongOut);
+    }
+
+    @Test
+    public void testPolluted() {
+        ArbitraryBasic effect = new ArbitraryBasic(List.of(car));
+
+        List<Resource> output = new ArrayList<>();
+        List<Resource> input = List.of(bulb, bulb);
+
+        boolean polluted = effect.check(input, output, 1);
+
+        assertFalse(polluted);
     }
 
     @Test
     public void testStateFormat() {
-        ArbitraryBasic effect = new ArbitraryBasic(Arrays.asList(car));
+        ArbitraryBasic effect = new ArbitraryBasic(List.of(car));
+        ArbitraryBasic effect1 = new ArbitraryBasic(List.of(car, bulb, car));
 
         String s = effect.state();
-        assertEquals("[(any 1 resources) -> (Bulb)]", s);
+        assertEquals("[(any 1 resources) -> (Car)]", s);
+
+        String s1 = effect1.state();
+        assertEquals("[(any 1 resources) -> (Car Bulb Car )]", s1);
     }
 
     @Test
     public void testAssistanceFlag() {
-        assertFalse(new ArbitraryBasic(Arrays.asList(bulb)).hasAssistance());
+        assertFalse(new ArbitraryBasic(List.of(bulb)).hasAssistance());
     }
 }
