4957ddedfeb8407511c71977b1d049f73abc9175
davidhrabcak <davidko.hrabcak@gmail.com>
2025-12-01 11:33:55 +0100

    add constructor in GridPosition.java

--- src/main/java/com/terrafutura/board/GridPosition.java
+++ src/main/java/com/terrafutura/board/GridPosition.java
@@ -1,5 +1,7 @@
 package main.java.com.terrafutura.board;
 
+import java.util.AbstractMap;
+
 public class GridPosition {
     private int x, y;
 
@@ -11,6 +13,18 @@ public class GridPosition {
             this.y = y;
         }
         if (x < -2 || x > 2 || y < -2 || y > 2) {
+            throw new IllegalArgumentException("Coordinates out of bounds.");
+        }
+    }
+
+    public GridPosition(AbstractMap.SimpleEntry<Integer, Integer> c) {
+        if (c.getKey() <= 2 && c.getKey() >= -2) {
+            this.x = c.getKey();
+        }
+        if (c.getValue() <= 2 && c.getValue() >= -2) {
+            this.y = c.getValue();
+        }
+        if (c.getKey() < -2 || c.getKey() > 2 || c.getValue() < -2 || c.getValue() > 2) {
             throw new IllegalArgumentException("Coordinates out of bounds.");
         }
     }
